4a5133b5f5c92969dcf15983f18fb6d340c6a042
Mohd Shahzil <130140298+MohdShahzil29@users.noreply.github.com>
2025-02-22 10:39:14 +0530

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import store from "./redux/AuthStore.js";
+import store from "./redux/authStore.js";
 import ScrollToTop from "./components/ScrollToTop.jsx";
 
 createRoot(document.getElementById("root")).render(
